1b10b7387dfd932cefba19e5524cb8a7a6083ed7
wangqinkuan <1345244694@qq.com>
2019-02-20 22:28:00 +0800

    实现管理员模块用户管理部分的后台部分功能

--- codes/web/idevtools/src/main/java/cn/idevtools/service/impl/UserServiceImpl.java
+++ codes/web/idevtools/src/main/java/cn/idevtools/service/impl/UserServiceImpl.java
@@ -32,6 +32,13 @@ public class UserServiceImpl implements UserService {
         return userTMapper.selectUserPage(pageParams);
     }
 
+    /**
+     * 使用分页插件获取全部用户的基本信息
+     * @param pageNum 页号
+     * @param pageSize 每页显示量
+     *
+     * @return 被PageInfo包装后的查询分页结果
+     */
     @Override
     public PageInfo<UserT> getAllUserPage(int pageNum, int pageSize) {
         PageHelper.startPage(pageNum,pageSize);
@@ -48,6 +55,11 @@ public class UserServiceImpl implements UserService {
         return userTMapper.updateUserValidById(user);
     }
 
+    /**
+     * 更新用户的基本信息
+     * @param user 待修改的用户对象
+     * @return 影响行数，修改用户只操作单个对象，故成功返回1
+     */
     @Override
     public int updateUser(UserT user) {
         return userTMapper.updateUser(user);
@@ -58,6 +70,14 @@ public class UserServiceImpl implements UserService {
         return 0;
     }
 
+    /**
+     * 查询符合条件的用户并分页
+     * @param user 待查询的用户对象
+     *
+     * @param pageNum
+     * @param pageSize
+     * @return 被PageInfo包装后的查询分页结果
+     */
     @Override
     public PageInfo<UserT> getUsersPage(UserT user, int pageNum, int pageSize) {
         PageHelper.startPage(pageNum,pageSize);
@@ -65,6 +85,12 @@ public class UserServiceImpl implements UserService {
         return new PageInfo<UserT>(users);
     }
 
+    /**
+     * 根据用户id得到附带标签信息的用户详细信息。
+     * 包含的属性字段有:用户的全部属性字段,用户对应标签的全部属性字段
+     * @param userId 用户id
+     * @return 附带标签信息的单个用户详细信息
+     */
     @Override
     public UserT getUserDetailWithTagById(Integer userId) {
         return userTMapper.selectUserDetailWithTagById(userId);
