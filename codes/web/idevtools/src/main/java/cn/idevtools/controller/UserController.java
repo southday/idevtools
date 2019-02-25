9d67818cd50a8ec85e57982bc178735bf2080acb
wangqinkuan <1345244694@qq.com>
2019-02-25 22:12:19 +0800

    完成UserTagController部分功能,重构测试模块

--- codes/web/idevtools/src/main/java/cn/idevtools/controller/UserController.java
+++ codes/web/idevtools/src/main/java/cn/idevtools/controller/UserController.java
@@ -12,8 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpSession;
 import java.util.List;
 
 /**
