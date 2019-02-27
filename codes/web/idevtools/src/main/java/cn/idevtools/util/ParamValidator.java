b4e7c8ec9d492e2ecc6356c6c67561cabcb53d0d
southday <lichaoxi7@qq.com>
2019-02-28 02:25:47 +0800

    ParamValidator增加Email地址合法性的校验

--- codes/web/idevtools/src/main/java/cn/idevtools/util/ParamValidator.java
+++ codes/web/idevtools/src/main/java/cn/idevtools/util/ParamValidator.java
@@ -1,11 +1,20 @@
 package cn.idevtools.util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * 参数校验器
  * @author southday
  * @date 2019/2/27
  */
 public class ParamValidator {
+    private static final Pattern emailPattern;
+
+    static {
+        String regx = "^\\w+((-\\w+)|(\\.\\w+))*\\@[A-Za-z0-9]+((\\.|-)[A-Za-z0-9]+)*\\.[A-Za-z0-9]+$";
+        emailPattern = Pattern.compile(regx);
+    }
 
     /**
      * 传入的参数是否所有都为非空字符串<br>
@@ -20,5 +29,14 @@ public class ParamValidator {
             if (p == null || "".equals(p.trim()))
                 return false;
         return true;
+    }
+
+    /**
+     * 验证邮箱是否合法
+     * @param email
+     * @return
+     */
+    public static boolean isEmailValid(String email) {
+        return notNullString(email) && emailPattern.matcher(email).matches();
     }
 }
