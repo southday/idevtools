e02a84d0df522beed79d0b5a4bfc6c5649a9f3fe
southday <lichaoxi7@qq.com>
2019-03-03 17:13:01 +0800

    重构Message部分，添加StatusCode来控制Code的范围（SUCCESS, FAILURE...）；相应地更新重构所影响的内容；

--- codes/web/idevtools/src/main/java/cn/idevtools/common/CodeMsgC.java
+++ codes/web/idevtools/src/main/java/cn/idevtools/common/CodeMsgC.java
@@ -1,31 +1,31 @@
 package cn.idevtools.common;
 
 /**
- * 状态码-消息 Class
+ * 状态码-消息 Class，使外部可new对象，作为预定义CodeMsgE的补充
  * 编码规则：
  * <pre>
- * 1) 成功 code = 1，失败 code = -1
+ * 1) 成功 code = StatusCode.SUCCESS，失败 code = StatusCode.FAILURE
  * 2) 其他特殊定义的code，如：
- *      VALID_ERROR(-8, "表单校验失败")
+ *      VALID_ERROR(StatusCode.VALID_ERROR, "表单校验失败")
  * </pre>
  * @author southday
  * @date 2019/2/28
  * @see CodeMsgE
  */
 public class CodeMsgC {
-    private int code;
+    private StatusCode code;
     private String msg;
 
-    public CodeMsgC(int code, String msg) {
+    public CodeMsgC(StatusCode code, String msg) {
         this.code = code;
         this.msg = msg;
     }
 
-    public int getCode() {
+    public StatusCode getCode() {
         return code;
     }
 
-    public void setCode(int code) {
+    public void setCode(StatusCode code) {
         this.code = code;
     }
 
