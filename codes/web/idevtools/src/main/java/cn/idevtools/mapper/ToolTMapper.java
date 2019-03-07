d4412f3f73495c0ff7a086001fbaa33544dee720
southday <lichaoxi7@qq.com>
2019-03-07 20:29:14 +0800

    添加CommonController：/view/toolDirs/sub/{dirId}，/tools/{toolId}实现

--- codes/web/idevtools/src/main/java/cn/idevtools/mapper/ToolTMapper.java
+++ codes/web/idevtools/src/main/java/cn/idevtools/mapper/ToolTMapper.java
@@ -1,5 +1,14 @@
 package cn.idevtools.mapper;
 
+import cn.idevtools.po.ToolT;
+
 public interface ToolTMapper {
 
+    /**
+     * 根据toolId查询指定工具
+     * southday 2019.03.07
+     * @param toolId
+     * @return
+     */
+    ToolT selectToolByToolId(Integer toolId);
 }
---
+++ codes/web/idevtools/src/main/java/cn/idevtools/service/CommonService.java
@@ -0,0 +1,32 @@
+package cn.idevtools.service;
+
+import cn.idevtools.po.ToolDirT;
+import cn.idevtools.po.ToolT;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 公共服务 Service，处理一些公共的请求
+ * @author southday
+ * @date 2019/3/7
+ */
+@Service
+public interface CommonService {
+
+    /**
+     * 获取指定dirId对应的子文件信息(工具、目录)
+     * southday 2019.03.07
+     * @param dirId
+     * @return
+     */
+    List<ToolDirT> getSubToolDirs(Integer dirId);
+
+    /**
+     * 通过toolId获取对应的工具信息
+     * southday 2019.03.07
+     * @param toolId
+     * @return
+     */
+    ToolT getToolByToolId(Integer toolId);
+}
